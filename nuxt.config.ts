7f9b77cf2bc6f4e32b45b8cca7ed8ba1c68bc7f0
Jesse Corbett <jesselcorbett@gmail.com>
2023-11-23 15:59:55 -0800

    Enable GH Pages

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  ssr: false,
   devtools: { enabled: true },
+
   modules: ["@pinia/nuxt", "@vueuse/nuxt", "@nuxtjs/tailwindcss"],
+
+  app: {
+    baseURL: "/dance-game/", // baseURL: '/<repository>/'
+    buildAssetsDir: "assets", // don't use "_" at the begining of the folder name to avoids nojkill conflict
+  },
 });
