caab0aa4c4563cbfe8be3af1818f26e50b6be3e8
Jesse Corbett <jesselcorbett@gmail.com>
2026-02-07 18:06:34 -0800

    Fix paths

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,7 +6,7 @@ export default defineNuxtConfig({
   modules: ["@pinia/nuxt", "@vueuse/nuxt", "@nuxtjs/tailwindcss"],
 
   app: {
-    baseURL: "/dance-game/", // baseURL: '/<repository>/'
+    baseURL: "/", // baseURL: '/<repository>/'
     buildAssetsDir: "assets", // don't use "_" at the begining of the folder name to avoids nojkill conflict
   },
 });
--- pages/index.vue
+++ pages/index.vue
@@ -71,13 +71,13 @@ async function loadDefaultCharts() {
   const defaultCharts = [
     {
       name: "Espresso",
-      path: "/dance-game/chart/Espresso/",
+      path: "/chart/Espresso/",
       file: "chart.ssc",
       format: "ssc" as const,
     },
     {
       name: "Suki Suki Daisuki",
-      path: "/dance-game/chart/Suki Suki Daisuki/",
+      path: "/chart/Suki Suki Daisuki/",
       file: "chart.ssc",
       format: "ssc" as const,
     },
