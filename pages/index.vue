8d76100c256d6c6d6d783f40bd8f6f38d5d689b4
Jesse Corbett <jesselcorbett@gmail.com>
2026-02-07 17:30:22 -0800

    Use relative paths

--- pages/index.vue
+++ pages/index.vue
@@ -71,13 +71,13 @@ async function loadDefaultCharts() {
   const defaultCharts = [
     {
       name: "Espresso",
-      path: "/chart/Espresso/",
+      path: "chart/Espresso/",
       file: "chart.ssc",
       format: "ssc" as const,
     },
     {
       name: "Suki Suki Daisuki",
-      path: "/chart/Suki Suki Daisuki/",
+      path: "chart/Suki Suki Daisuki/",
       file: "chart.ssc",
       format: "ssc" as const,
     },
